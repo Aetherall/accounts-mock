f9ae6aab85cc782f7976bc779955642419b7c6a2
Aetherall <elieslou.dev@gmail.com>
2017-12-03 15:57:25 +0100

    Added Facebook OAuthProvider

---
+++ AuthenticationProviderFacebook/src/AuthenticationProviderFacebook.ts
@@ -0,0 +1,29 @@
+import { AuthenticationProviderOAuth } from '../../Types/AuthenticationProviderOAuth';
+
+import requestPromise from 'request-promise';
+
+class AuthenticationProviderOAuthFacebook implements AuthenticationProviderOAuth {
+
+  public name: string = 'facebook';
+
+  constructor(){
+
+  }
+  
+  authenticate = async (params) => {
+    if (!params.access_token) {
+      throw new Error('No access token provided');
+    }
+    const user = await requestPromise({
+      method: 'GET',
+      json: true,
+      uri: 'https://graph.facebook.com/v2.9/me',
+      qs: { access_token: params.access_token },
+    });
+    // TODO get user email and save it
+    return {
+      id: user.id,
+      name: user.name,
+    };
+  }
+}
